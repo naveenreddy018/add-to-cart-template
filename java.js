a02d986f4ba09e2b681d2946ccce06e1a6710fb9
naveenreddy018 <naveenreddy310744@gmail.com>
2024-09-27 18:37:32 +0530

    firstchange

---
+++ java.js
@@ -0,0 +1,19 @@
+/* lists = [{"name": "naveen redd",
+    "vlass" : " 41540",
+    "phno " : "7893467045"
+},
+{"name": "moni redd",
+    "vlass" : " 540",
+    "phno " : "10893467045"
+},
+{"name": "n redd",
+    "vlass" : " /40",
+    "phno " : "2558//8045"
+}]
+console.log(lists.name)
+for(obj of lists){
+    console.log(obj)
+    for(key in obj){
+        console.log(obj[key])
+    }
+} */
